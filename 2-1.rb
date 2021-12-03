2c200774e580290d44005b7ebabf763517d90f0f
Tim Tilberg <ttilberg@gmail.com>
2021-12-03 11:39:26 -0600

    2-1

---
+++ 2-1.rb
@@ -0,0 +1,30 @@
+class WaterBilly
+  attr_reader :depth, :x
+
+  def initialize
+    @depth = 0
+    @x = 0
+  end
+
+  def forward(distance)
+    @x += distance
+  end
+
+  def up(distance)
+    @depth -= distance
+  end
+
+  def down(distance)
+    @depth += distance
+  end
+end
+
+gizmo = WaterBilly.new
+
+File.read('data/day-2-input.txt').each_line do |instruction|
+  direction, amplitude = instruction.split(' ').map(&:strip)
+  gizmo.public_send(direction, amplitude.to_i)
+end
+
+puts "Yar, we bein' #{gizmo.depth} undah 'n #{gizmo.x} thingars away."
+puts "For som'n reas'n we'n gun' multiplay thars t'gthars. She blars ar #{gizmo.depth * gizmo.x}!"
