c2bae75985d5cf1b4d3e4c63133c59194579474b
Tim Tilberg <ttilberg@gmail.com>
2021-12-03 16:38:52 -0600

    Instead of calling #reverse mid chain, just reorg the vars

--- 3-1.rb
+++ 3-1.rb
@@ -15,12 +15,12 @@ class DiagnosticReport
 
   # Transpose the bit array so we can scan each position
   # For each position, the `gamma` bit is whatever bit has highest frequency,
-  #   and the `epsilson` bit is the other.
+  #   and the `epsilon` bit is the other.
   # Convert the bits back to integers.
   def scan!
-    @gamma, @epsilon = report.transpose.map do |bits|
+    @epsilon, @gamma = report.transpose.map do |bits|
       frequency = bits.tally
-      frequency.sort_by {|_bit, freq| freq }.reverse.map(&:first)
+      frequency.sort_by {|_bit, freq| freq }.map(&:first)
     end.transpose.map(&:join).map{|bits| bits.to_i(2)}
   end
 end
