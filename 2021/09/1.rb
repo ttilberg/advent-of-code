9f7d6ca64251df8efd3e68682f23b2f309849393
Tim Tilberg <ttilberg@gmail.com>
2021-12-09 17:35:29 -0600

    Fixed the world-wrapping error.

--- 2021/09/1.rb
+++ 2021/09/1.rb
@@ -1,35 +1,19 @@
-sample = <<~TXT
-2199943210
-3987894921
-9856789892
-8767896789
-9899965678
-TXT
+UPPY_DOWNY_LEFTY_RIGHTY = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-# board = sample.split("\n").map{|line| line.chars.map(&:to_i)}
-board = File.read('input.txt').split("\n").map{|line| line.chars.map(&:to_i)}
+board = File.read('input.txt').split("\n").map.with_index do |line, y|
+  line.chars.map.with_index do |char, x|
+    {[x, y] => char.to_i}
+  end
+end.flatten.reduce(&:merge)
 
-board_x = board.first.size - 1
-board_y = board.size - 1
+board.default = 10 # A number higher than 9. (For the borders.)
 
 risk_level = 0
 
-(0..board_x).to_a.each_index do |x|
-  (0..board_y).to_a.each_index do |y|
-    # Even though we have a nice looking [x,y] point, the fields are flipped on the board: rows (y) come first.
-    this = board[y][x]
-    if this < (board[y-1]&.[](x) || 10) &&
-       this < (board[y+1]&.[](x) || 10) &&
-       this < (board[y]&.[](x+1) || 10) &&
-       this < (board[y]&.[](x-1) || 10)
-      risk_level += this + 1
-    end
+board.each do |(x, y), val|
+  if UPPY_DOWNY_LEFTY_RIGHTY.all? {|⇦⇨, ⇧⇩| board[[x + ⇦⇨, y + ⇧⇩]] > val }
+    risk_level += val + 1
   end
 end
 
 puts risk_level
-
-raise "It turns out, this solution has a board wraparound bug,
-  where if it's on the boarder, it scans to the opposite side of the board.
-
-  It passes the answer test, but I think that's by luck."
