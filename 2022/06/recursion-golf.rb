ea807ae256c9b24b29331bd0182a7bbc1822c11d
Tim Tilberg <tim@fitmentgroup.com>
2022-12-06 11:22:24 -0600

    🏌

---
+++ 2022/06/recursion-golf.rb
@@ -0,0 +1,10 @@
+p (🏌=->s,b,i=0{return i if b.slice(i,s).uniq.size==s;🏌[s,b,i+1]})[14,$<.read.bytes]
+
+# Similar to:
+# def go! size, input, start=0
+#   return start if input.slice(start, size).uniq.count == size
+#   go! size, input, start += 1
+# end
+
+
+# p go! 14, File.read("input.txt").bytes
