7fa05b00ff4f64e25aeae2f3eb1144cf098963cf
Tim Tilberg <tim@fitmentgroup.com>
2022-12-14 18:34:51 -0600

    day 10

--- 2022/10/1.rb
+++ 2022/10/1.rb
@@ -1,50 +1,77 @@
-class World
+class Gizmo
+  WIDTH = 40
+
   attr_reader :tick_count
   attr_reader :signal
   def initialize
     @tick_count = 0
-
-    @cmds = File.read("input.txt").split("\n")
-
+    @stack = []
     @signal = 1
-    @cpu = []
+
+    parse_input!
   end
 
   def tick
     @tick_count += 1
-    val = @cpu.shift || 0
-    return if busy?
-    
-    @signal += val
+    yield self if block_given?
+    @signal += @stack.shift
+  end
 
-    case @cmds.shift.split
-    in ["noop"]
-      @cpu << 0
-    in ["addx", /[-0-9+]/ => val]
-      @cpu += [0, val.to_i]
+  def output
+    val = nsync? ? '#' : '.'
+    val << "\n" if x_cursor == WIDTH - 1
+    val
+  end
+
+  def complete?
+    @stack.empty?
+  end
+
+  private
+
+  # The command list can act as a stack to shift each tick.
+  #
+  # A no-op cmd having a cooldown of 1 round is the same
+  # as shifting off a 0 to add to the signal.
+  #
+  # Similarly, an add command takes two rounds, so
+  # this is the same as [0, val]
+  #
+  # This means we can just always add 0 for each line,
+  # and if an int exists, add that too.
+  #
+  def parse_input!
+    File.read("input.txt").split("\n").each do |line|
+      @stack << 0
+      val = line[/[-0-9]+/]
+      @stack << val.to_i if val
     end
   end
 
-  def busy?
-    @cpu.any?
+  def pixels
+    [-1, 0, 1].map{|spread| signal + spread}
   end
 
-  def signal_str
-    @signal * @tick_count
+  def x_cursor
+    (tick_count - 1) % WIDTH
+  end
+
+  def nsync?
+    pixels.include? x_cursor
   end
 end
 
-
-world = World.new
-key_cycles = [20, 60, 100, 140, 180, 220]
+gizmo = Gizmo.new
+key_frames = [20, 60, 100, 140, 180, 220]
 key_values = []
-until world.tick_count > 220
-  world.tick
-  p tick: world.tick_count, signal: world.signal, str: world.signal_str
-  if key_cycles.include? world.tick_count
-    p tick: world.tick_count, signal: world.signal
-    key_values << world.signal_str
+
+until gizmo.complete?
+  gizmo.tick do |gizmo|
+    print gizmo.output
+    if key_frames.include? gizmo.tick_count
+      key_values << gizmo.tick_count * gizmo.signal
+    end
   end
 end
 
-p key_values.sum
+puts key_values.sum
