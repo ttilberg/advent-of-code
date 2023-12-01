c0cd757054674c341e748addbc75fbfb3d2a8c1b
Tim Tilberg <ttilberg@gmail.com>
2023-12-01 01:16:27 -0600

    More regex, less reversey

--- 2023/01/2.rb
+++ 2023/01/2.rb
@@ -1,23 +1,13 @@
 map = %w[zero one two three four five six seven eight nine].each.with_index.to_h.transform_values(&:to_s)
-backmap = map.transform_keys(&:reverse)
 
-forward = Regexp.union(/\d/, *map.keys)
-backward = Regexp.union(/\d/, *backmap.keys)
+pattern = Regexp.union(/\d/, *map.keys)
 
 val = File.read('input.txt').lines.sum do |line|
-  a = line[forward]
-
-  # This step was nutty. `twone`. You rascal.
-  # But the crazier thing is... if it's at the end, then `two` matches first, becoming `2ne`, skipping the one.
-  # so I guess we should scan in reverse? So weird.
-  # I tried finding if there was a "don't consume matches"
-  # Looking forward to see how other people do this...
-  b = line.reverse[backward]
-
-  b = a if b.nil?
+  a = line[pattern]
+  b = line[/.*(#{pattern})/, 1]
 
   a = map[a] || a
-  b = backmap[b] || b
+  b = map[b] || b
 
   (a + b).to_i
 end
