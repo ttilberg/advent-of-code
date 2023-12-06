6e389c1e1aa90822b78978307a2254c0cda13027
Tim Tilberg <tim@fitmentgroup.com>
2023-12-06 11:21:34 -0600

    Don't need to assign `score` in retrospect.

--- 2023/06/1.rb
+++ 2023/06/1.rb
@@ -3,14 +3,12 @@ times, distances = File.read('input.txt').lines
                   .map(&:to_i)}
 
 scores = times.zip(distances).map do |time, record_distance|
-  score = time.times.to_a.count do |hold_time|
+  time.times.to_a.count do |hold_time|
     travel_time = time - hold_time
     distance = hold_time * travel_time
 
     distance > record_distance
   end
-
-  score
 end
 
 p scores.reduce(&:*)
