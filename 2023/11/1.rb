524cbd712ec7e3e369900e4e3758403157a91e99
Tim Tilberg <ttilberg@gmail.com>
2023-12-11 16:16:23 -0600

    Day 11

---
+++ 2023/11/1.rb
@@ -0,0 +1,16 @@
+MAP = {}
+File.read('input.txt').lines.each.with_index do |line, y|
+  line.chars.each.with_index do |char, x|
+    MAP[[x, y]] = true if char == '#'
+  end
+end
+
+x_list, y_list = MAP.keys.transpose.map(&:uniq)
+
+MAP.keys.combination(2).sum do |(lx, ly), (rx, ry)|
+  x_expansion = (Range.new(*[lx, rx].sort).to_a - x_list).count
+  y_expansion = (Range.new(*[ly, ry].sort).to_a - y_list).count
+
+  (rx - lx).abs + (ry - ly).abs + x_expansion + y_expansion
+end
+  .then{|sum| p sum}
